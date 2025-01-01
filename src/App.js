edf4d66e4ade8f36017bb99803926e320b8f66e9
Rama1432 <53965826+Rama1432@users.noreply.github.com>
2025-01-01 15:57:18 +1100

    Changed the contact page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
           <ul>
             <li><a href="/">Home</a></li>
             <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <li><a href="/contacts">Contact</a></li>
           </ul>
         </nav>
         <h1>Welcome to React</h1>
